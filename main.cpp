d8055a9d4a5d91199a772bafe6eb8bfeaeab9cec
Kiril Macuk <kirmac23@gmail.com>
2026-03-02 21:26:42 +0200

    pradetas pirmas darbas

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//valiutos kursas
+const float GBP_Bendras   = 0.8729;
+const float GBP_Pirkti    = 0.8600;
+const float GBP_Parduoti  = 0.9220;
+const float USD_Bendras   = 1.1793;
+const float USD_Pirkti    = 1.1460;
+const float USD_Parduoti  = 1.2340;
+const float INR_Bendras   = 104.6918;
+const float INR_Pirkti    = 101.3862;
+const float INR_Parduoti  = 107.8546;
+int main () {
+    string pavadinimas;
+    float bendras,pirkti,parduoti,vnt,sum;
+    int valiuta , veiksmas;
+    cout << "prasau ivesti valiuta" << endl;
+    cout << "1 GBP \n "  << "2 USD \n " << "3 INR" << endl;
+     cin >> valiuta;
+return 0;
+    }
