2d51ef354f034faca6c7babe852abfc45ec9e80d
Kiril Macuk <kirmac23@gmail.com>
2026-03-05 20:14:31 +0200

    pabaigaiu uzdavini ir pridejau cikla while,kad butu galima grizt atgal

--- main.cpp
+++ main.cpp
@@ -11,34 +11,69 @@ const float INR_Bendras   = 104.6918;
 const float INR_Pirkti    = 101.3862;
 const float INR_Parduoti  = 107.8546;
 int main () {
-    string pavadinimas;
-    float bendras,pirkti,parduoti,vnt,sum;
-    int valiuta , veiksmas;
-    cout << "prasau ivesti valiuta" << endl;
-    cout << "1 GBP \n "  << "2 USD \n " << "3 INR" << endl;
-     cin >> valiuta;
-return 0;
+    int pasiriknkimas = 1;
+    while (pasiriknkimas == 1) {
+        string pavadinimas;
+        float bendras,pirkti,parduoti,vnt,sum;
+        int valiuta , veiksmas;
+        cout << "prasau ivesti valiuta" << endl;
+        cout << "1 GBP \n "  << "2 USD \n " << "3 INR" << endl;
+        cin >> valiuta;
+
+        cout << " Norimi veiksmai " << endl;
+        cout << "1 valiutos kursas \n" << "2 nusipirkti valiutos \n " << "3 parduot valiutos" << endl;
+        cin >> veiksmas;
+        switch (valiuta) {
+            case 1 :
+                pavadinimas = "GBP";
+                bendras = GBP_Bendras;
+                pirkti = GBP_Pirkti;
+                parduoti = GBP_Parduoti;
+                break;
+            case 2 :
+                pavadinimas = "USD";
+                bendras = USD_Bendras;
+                pirkti = USD_Pirkti;
+                parduoti = USD_Parduoti;
+                break;
+            case 3 :
+                pavadinimas = "INR";
+                bendras = INR_Bendras;
+                pirkti = INR_Pirkti;
+                parduoti = INR_Parduoti;
+                break;
+            default :
+                cout << " Neteisingas ivedimas" << endl;
+                return 0;
+        }
+        if (veiksmas == 1) {
+            cout << "valiutos" << pavadinimas << " kursas : \n";
+            cout << "bendras kursas " << bendras << endl;
+            cout << "pirkti kursas " << pirkti << endl;
+            cout << "parduoti kursas " << parduoti << endl;
+        }
+        else if (veiksmas == 2 ) {
+            cout << "keik norit pakeist ";
+            cin >> vnt;
+            sum = pirkti * vnt;
+            cout << "Jus Gausit " << sum << endl;
+        }
+        else if (veiksmas == 3 ) {
+            cout << "Kiek norit parduot " << pavadinimas << endl;
+            cout << pavadinimas;
+            cin >> vnt;
+            sum = vnt *1. / parduoti;
+            // klaida
+            cout << "Gausit " << sum << " eur" <<  endl;
+        }
+        else {
+            cout << " tokios paslaugos neturim " << endl;
+        }
+
+        cout << " \n1 - grizti i pradzia" << endl;
+        cout << " 0 - baigti programa" << endl;
+        cin >> pasiriknkimas;
     }
-switch (valiuta) {
-    case 1 :
-        pavadinimas = "GBP";
-        bendras = GBP_Bendras;
-        pirkti = GBP_Pirkti;
-        parduoti = GBP_Parduoti;
-        break;
-    case 2 :
-        pavadinimas = "USD";
-        bendras = USD_Bendras;
-        pirkti = USD_Pirkti;
-        parduoti = USD_Parduoti;
-        break;
-    case 3 :
-        pavadinimas = "INR";
-        bendras = INR_Bendras;
-        pirkti = INR_Pirkti;
-        parduoti = INR_Parduoti;
-        break;
-    default :
-        cout << " Neteisingas ivedimas" << endl;
-        return 0;
+    return 0;
 }
+
