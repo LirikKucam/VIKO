8fe49562e6a0b07b727c57c46e881b2d0aa4ca57
Kiril Macuk <kirmac23@gmail.com>
2026-04-02 00:58:26 +0300

    Priedejau case 3 ir case 4

--- 2uzdt/main.cpp
+++ 2uzdt/main.cpp
@@ -19,9 +19,7 @@ int main() {
         cout << "2 - Rodyti visus\n";
         cout << "3 - Rodyti viena\n";
         cout << "4 - Keisti pazymi\n";
-        cout << "5 - Trinti mokini\n";
         cout << "0 - Iseiti\n";
-        cout << ">>> ";
         cin >> pasirinkimas;
 
         switch (pasirinkimas) {
@@ -73,4 +71,75 @@ int main() {
             }
             break;
         }
+            case 3: {
+            string v;
+            bool rasta = false;
+
+            cout << "Vardas: ";
+            cin >> v;
+
+            for (int i = 0; i < mokKiekis; i++) {
+                if (vardai[i] == v) {
+                    cout << vardai[i] << ": ";
+                    for (int j = 0; j < kiekPaz[i]; j++) {
+                        cout << pazymiai[i][j] << " ";
+                    }
+                    cout << endl;
+                    rasta = true;
+                    break;
+                }
+            }
+
+            if (!rasta) {
+                cout << "Nerasta.\n";
+            }
+            break;
+            }
+
+            case 4: {
+            string v;
+            bool rasta = false;
+
+            cout << "Vardas: ";
+            cin >> v;
+
+            for (int i = 0; i < mokKiekis; i++) {
+                if (vardai[i] == v) {
+
+                    for (int j = 0; j < kiekPaz[i]; j++) {
+                        cout << j + 1 << ":" << pazymiai[i][j] << " ";
+                    }
+                    cout << endl;
+
+                    int nr, naujas;
+                    cout << "Kurio nr keisti: ";
+                    cin >> nr;
+
+                    if (nr < 1 || nr > kiekPaz[i]) {
+                        cout << "Blogas numeris!\n";
+                        rasta = true;
+                        break;
+                    }
+
+                    cout << "Naujas pazymys: ";
+                    cin >> naujas;
+
+                    if (naujas < 1 || naujas > 10) {
+                        cout << "Blogas pazymys!\n";
+                        rasta = true;
+                        break;
+                    }
+
+                    pazymiai[i][nr - 1] = naujas;
+                    cout << "Pakeista!\n";
+                    rasta = true;
+                    break;
+                }
+            }
+
+            if (!rasta) {
+                cout << "Nerasta.\n";
+            }
+            break;
+            }
 
